8c54f0e42dfc8b6e7dc1a24057ac6de4f17a62cb
jhon186 <54211040+jhon186@users.noreply.github.com>
2022-04-14 14:50:15 +0800

    Update JavDBCrack.js

--- JavDBCrack.js
+++ JavDBCrack.js
@@ -15,7 +15,7 @@
 *******************************
 [rewrite_local]
 # > JavDB解锁会员
-^https?:\/\/(shukego|yiliang8888)\.com\/api\/v1\/movies\/.*\/play url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/JavDBCrack.js
+^https?:\/\/(shukego|yiliang8888)\.com\/api\/v1\/movies\/.*\/play url script-request-header https://raw.githubusercontent.com/jhon186/QuantumultX-1/master/JavDBCrack.js
 
 [mitm] 
 hostname = shukego.com, yiliang8888.com
